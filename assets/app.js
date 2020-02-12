7c513a9e31fee3038c9d313f9a88d5d52886b469
mjimenez05 <matthewjjimenez05@hotmail.com>
2020-02-11 21:34:57 -0700

    Broke the hw cont figure out why, this is for you mike

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,67 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyATWmVcBQbfB9I7ykBltwe4FJPwB-n0BGE",
-    authDomain: "train-scheduler-9c3cf.firebaseapp.com",
-    databaseURL: "https://train-scheduler-9c3cf.firebaseio.com",
-    projectId: "train-scheduler-9c3cf",
-    storageBucket: "train-scheduler-9c3cf.appspot.com",
-    messagingSenderId: "1022083629588",
-    appId: "1:1022083629588:web:dfa7cf91fc0efa2e561d01",
-    measurementId: "G-JEH01YRE8T"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+$(document).ready(function() {
+    var config = {
+        apiKey: "AIzaSyATWmVcBQbfB9I7ykBltwe4FJPwB-n0BGE",
+        authDomain: "train-scheduler-9c3cf.firebaseapp.com",
+        databaseURL: "https://train-scheduler-9c3cf.firebaseio.com",
+        projectId: "train-scheduler-9c3cf",
+        storageBucket: "train-scheduler-9c3cf.appspot.com",
+        messagingSenderId: "1022083629588",
+        appId: "1:1022083629588:web:dfa7cf91fc0efa2e561d01",
+        measurementId: "G-JEH01YRE8T"
+    };
+    firebase.initializeApp(config);
+    firebase.analytics();
+    // A variable to reference the database.
+    var database = firebase.database();
+    // Variables for the onClick event
+    var name;
+    var destination;
+    var firstTrain;
+    var frequency = 0;
+    $("#add-train").on("click", function() {
+        event.preventDefault();
+        // Storing and retreiving new train data
+        name = $("#train-name").val().trim();
+        destination = $("#destination").val().trim();
+        firstTrain = $("#first-train").val().trim();
+        frequency = $("#frequency").val().trim();
+        // Pushing to database
+        database.ref().push({
+            name: name,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        $("form")[0].reset();
+    });
+    database.ref().on("child_added", function(childSnapshot) {
+        var nextArr;
+        var minAway;
+        // Chang year so first train comes before now
+        var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+        // Difference between the current and firstTrain
+        var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+        var remainder = diffTime % childSnapshot.val().frequency;
+        // Minutes until next train
+        var minAway = childSnapshot.val().frequency - remainder;
+        // Next train time
+        var nextTrain = moment().add(minAway, "minutes");
+        nextTrain = moment(nextTrain).format("hh:mm");
+        $("#add-row").append("<tr><td>" + childSnapshot.val().name +
+            "</td><td>" + childSnapshot.val().destination +
+            "</td><td>" + childSnapshot.val().frequency +
+            "</td><td>" + nextTrain +
+            "</td><td>" + minAway + "</td></tr>");
+        // Handle the errors
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
+    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+        // Change the HTML to reflect
+        $("#name-display").html(snapshot.val().name);
+        $("#email-display").html(snapshot.val().email);
+        $("#age-display").html(snapshot.val().age);
+        $("#comment-display").html(snapshot.val().comment);
+    });
+});

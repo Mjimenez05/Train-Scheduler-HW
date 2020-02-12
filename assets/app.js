d469870202a7516263b26ced0f4486f0abe8c6a5
mjimenez05 <matthewjjimenez05@hotmail.com>
2020-02-11 20:10:11 -0700

    added firebase api key updated html and css

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyATWmVcBQbfB9I7ykBltwe4FJPwB-n0BGE",
+    authDomain: "train-scheduler-9c3cf.firebaseapp.com",
+    databaseURL: "https://train-scheduler-9c3cf.firebaseio.com",
+    projectId: "train-scheduler-9c3cf",
+    storageBucket: "train-scheduler-9c3cf.appspot.com",
+    messagingSenderId: "1022083629588",
+    appId: "1:1022083629588:web:dfa7cf91fc0efa2e561d01",
+    measurementId: "G-JEH01YRE8T"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
